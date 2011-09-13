ecb72b2cdf65fc0e472b6a42de11526ea4be6724
unknown <Admin@.(none)>
2011-09-12 21:38:57 -0400

    Création répertoire /include/user pour fichiers à inclure dans /include/template/main.tpl

---
+++ include/user/menugauche.php
@@ -0,0 +1,102 @@
+<div id="menugauche">
+
+<p class=" ">
+<img src="/wiki/lib/exe/fetch.php?w=20&amp;media=menu:home1_mini.png" class="media" alt="" width="20"><strong><a href="/wiki/doku.php?id=accueil" class="wikilink1" title="accueil">Accueil</a> </strong>
+
+</p>
+<hr class=" ">
+
+<p class=" ">
+<img src="/wiki/lib/exe/fetch.php?w=20&amp;media=menu:interrogation.png" class="media" alt="" width="20"><strong><a href="/wiki/doku.php?id=presentation:sommaire" class="wikilink1" title="presentation:sommaire">Présentation</a> </strong>
+
+</p>
+
+<hr class=" ">
+<ul class=" ">
+<li class="level1"><div class="li"> <a href="/wiki/doku.php?id=presentation:debuter" class="wikilink1" title="presentation:debuter">Débuter</a> </div>
+</li>
+<li class="level2"><div class="li"> <a href="/wiki/doku.php?id=faq_de_la_randonnee_legere_mul" class="wikilink1" title="faq_de_la_randonnee_legere_mul">Faq</a></div>
+</li>
+</ul>
+
+<p>
+
+<img src="/wiki/lib/exe/fetch.php?w=25&amp;media=menu:minilogolbleu.png" class="media" alt="" width="25"><strong><a href="/wiki/doku.php?id=association_rl" class="wikilink1" title="association_rl">L'Association</a> </strong>
+
+</p>
+<hr>
+<ul>
+<li class="level1"><div class="li"> <a href="/wiki/doku.php?id=association_adhesion" class="wikilink1" title="association_adhesion">Adhésion à l'asso</a> </div>
+</li>
+<li class="level2"><div class="li"> <a href="/wiki/doku.php?id=camps_de_base" class="wikilink1" title="camps_de_base">Camps de base</a></div>
+</li>
+<li class="level2"><div class="li"> <a href="http://www.randonner-leger.org/forum/viewtopic.php?pid=194231#p194231" class="urlextern" title="boutique">Boutique</a></div>
+</li>
+</ul>
+
+<p class=" ">
+
+<img src="/wiki/lib/exe/fetch.php?w=20&amp;media=menu:wiki.png" class="media" alt="" width="20"><strong><a href="/wiki/doku.php?id=accueil_du_wiki" class="wikilink1" title="accueil_du_wiki">Wiki</a> </strong>
+
+</p>
+<hr class=" ">
+
+<p class=" ">
+<em><a href="/wiki/doku.php?do=recent" class="urlextern" title="http://www.randonner-leger.org/wiki/doku.php?do=recent" rel="nofollow">Derniers changements</a> </em>
+
+</p>
+<ul class=" ">
+<li class="level1"><div class="li"> <a href="/wiki/doku.php?id=sommaire_fiches_conseil" class="wikilink1" title="sommaire_fiches_conseil">Conseils</a> </div>
+</li>
+<li class="level2"><div class="li"> <a href="/wiki/doku.php?id=sommaire_bricolage" class="wikilink1" title="sommaire_bricolage">Bricolage</a> </div>
+
+</li>
+<li class="level2"><div class="li"> <a href="/wiki/doku.php?id=mesures_du_poids_du_materiel" class="wikilink1" title="mesures_du_poids_du_materiel">Poids</a> </div>
+</li>
+<li class="level2"><div class="li"> <a href="/wiki/doku.php?id=sommaire_selection_materiel" class="wikilink1" title="sommaire_selection_materiel">Sélection matériel</a> </div>
+</li>
+<li class="level2"><div class="li"> <a href="/wiki/doku.php?id=lexique" class="wikilink1" title="lexique">Lexique</a> </div>
+</li>
+<li class="level2"><div class="li"> <a href="/wiki/doku.php?id=sommaire_culture" class="wikilink1" title="sommaire_culture">Culture/Ressources</a></div>
+
+</li>
+</ul>
+
+<p class=" ">
+
+<img src="/wiki/lib/exe/fetch.php?w=20&amp;media=menu:forum.png" class="media" alt="" width="20"><strong><a href="/forum/" class="urlextern" title="http://www.randonner-leger.org/forum/" rel="nofollow">Forum</a> </strong>
+
+</p>
+<hr class=" ">
+
+<p class=" ">
+<em><a href="/forum/search.php?action=show_24h" class="urlextern" title="http://www.randonner-leger.org/forum/search.php?action=show_24h" rel="nofollow">Messages récents</a> </em>
+
+</p>
+<ul class=" ">
+<li class="level1"><div class="li"> <a href="/forum/viewtopic.php?id=10299" class="urlextern" title="http://www.randonner-leger.org/forum/viewtopic.php?id=10299" rel="nofollow">Les sujets clés</a> </div>
+</li>
+<li class="level2"><div class="li"> <a href="/forum/search.php" class="urlextern" title="http://www.randonner-leger.org/forum/search.php" rel="nofollow">Recherche</a></div>
+</li>
+</ul>
+
+<p class=" ">
+
+<img src="/wiki/lib/exe/fetch.php?w=25&amp;media=menu:notice.png" class="media" alt="" width="25"><strong><a href="/wiki/doku.php?id=guide:sommaire" class="wikilink1" title="guide:sommaire">Guide d'utilisation</a> </strong>
+
+</p>
+<hr class=" ">
+<ul class=" ">
+<li class="level1"><div class="li"> <a href="/wiki/doku.php?id=guide:charte_forum" class="wikilink1" title="guide:charte_forum">Charte du forum</a> </div>
+</li>
+<li class="level2"><div class="li"> <a href="/wiki/doku.php?id=guide:notice_des_fonctionnalites_du_forum" class="wikilink1" title="guide:notice_des_fonctionnalites_du_forum">Notice du forum</a> </div>
+</li>
+<li class="level2"><div class="li"> <a href="/wiki/doku.php?id=guide:charte_wiki" class="wikilink1" title="guide:charte_wiki">Charte du wiki</a> </div>
+
+</li>
+<li class="level2"><div class="li"> <a href="/wiki/doku.php?id=guide:notice_du_wiki" class="wikilink1" title="guide:notice_du_wiki">Notice du wiki</a></div>
+</li>
+</ul>
+
+
+</div>
